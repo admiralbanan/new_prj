57f5665652768cedc00ea6a13b09a0af19655ca8
Alexander Kurochkin <110339661+admiralbanan@users.noreply.github.com>
2024-09-14 15:59:57 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,21 @@
+# variables.py
+
+# Описание объекта "сотрудник"
+name = "Иван Иванов"  # строка
+age = 35  # целое число
+salary = 75000.50  # число с плавающей точкой
+is_manager = True  # логический тип
+
+# Дополнительные переменные
+department = "Маркетинг"  # строка
+years_of_experience = 10  # целое число
+on_probation = False  # логический тип
+
+# Выводим тип каждой переменной
+print(type(name))
+print(type(age))
+print(type(salary))
+print(type(is_manager))
+print(type(department))
+print(type(years_of_experience))
+print(type(on_probation))
